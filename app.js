d131968e81ad91c288004174b7a23448ca65bc13
Ali Reza <99729607+AliReza1083@users.noreply.github.com>
2022-07-29 10:14:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ulNavbar = document.querySelector('.item2');
 const ulList = document.querySelector('.item2 ul');
 const openUl = document.querySelector('.ham');
+const nav__4 = document.querySelector('#nav__4');
 
 const closeUl = document.querySelector('.close');
 
@@ -10,14 +11,17 @@ openUl.addEventListener('click', e => {
         ulList.style.transform = 'translateX(0)'
     }, 1);
     document.body.style.overflow = 'hidden';
+    // nav__4.style.zIndex = '-1'; 
 })
 
 closeUl.addEventListener('click', e => {
     ulList.style.transform = 'translateX(100%)';
     setTimeout(() => {
-        ulNavbar.style.display = 'none'
+        ulNavbar.style.display = 'none';
+        nav__4.style.zIndex = '9999'; 
     }, 300);
     document.body.style.overflow = 'auto';
+
 })
 
 
